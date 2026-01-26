40f8e6d66e5e04257e8f8d29846e6b899f010402
blackcatin <umifirlana4@gmail.com>
2026-01-26 13:02:31 +0700

    fix hero

--- resources/js/components/services/Hero.tsx
+++ resources/js/components/services/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AnimatedContent from '@/components/Animation/AnimatedContent';
 import { Code2, Palette, Cpu, Cloud, Zap, Globe } from 'lucide-react';
 import { Card } from '@/components/services/FloatingCard';
+import RotatingText from '../Animation/RotatingText';
 
 export default function ServiceHero() {
     return (
@@ -42,7 +43,7 @@ export default function ServiceHero() {
                 sub="Scalable System"
                 variant="yellow"
                 shape="pill"
-                pos="right-[14%] bottom-[32%] -rotate-3"
+                pos="right-[5%] bottom-[40%] -rotate-3"
                 delay="1.5s"
                 icon={<Cloud size={18} />}
             />
@@ -52,7 +53,7 @@ export default function ServiceHero() {
                 sub="Efficiency First"
                 variant="red"
                 shape="leaf"
-                pos="center-[12%] top-[15%] -rotate-6 scale-110" 
+                pos="center-[12%] top-[15%] -rotate-6 scale-110"
                 delay="1.8s"
                 icon={<Zap size={18} />}
             />
@@ -66,22 +67,43 @@ export default function ServiceHero() {
             <main className="relative z-10 max-w-6xl mx-auto">
                 <AnimatedContent distance={40} direction="vertical" duration={1}>
                     <div className="flex items-center justify-center gap-2 mb-6">
-                        <h3 className="text-brand-yellow font-bold tracking-[0.3em] uppercase text-xs">
-                            Mission: <span className="text-white">Innovation</span>
+                        <h3 className="text-brand-yellow font-bold tracking-[0.3em] uppercase text-[10px] md:text-xs">
+                            Mission: <span className="text-white">Build Better Systems</span>
                         </h3>
                     </div>
 
-                    <h1 className="text-5xl md:text-[7rem] font-black tracking-tighter leading-[0.95] text-white uppercase italic mb-8">
-                        Elevating <br />
-                        <span className="text-brand-yellow">Digital Era</span>
+                    <h1 className="text-5xl md:text-[6rem] font-black tracking-tighter leading-[0.85] text-white uppercase italic mb-8 flex flex-col items-center">
+                        <span className="block">We Build</span>
+                            <div className="relative inline-block">
+                                <RotatingText
+                                    texts={[
+                                        'Frontend',
+                                        'Backend',
+                                        'Fullstack',
+                                        'Mobile',
+                                        'UI/UX',
+                                        'AI & Data',
+                                        'DevOps',
+                                        'Product'
+                                    ]}
+                                    mainClassName="px-4 bg-brand-yellow text-brand-darkbrown overflow-hidden py-1 md:py-2 justify-center rounded-xl md:rounded-2xl inline-flex"
+                                    staggerFrom={"last"}
+                                    initial={{ y: "100%" }}
+                                    animate={{ y: 0 }}
+                                    exit={{ y: "-120%" }}
+                                    staggerDuration={0.02}
+                                    transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                                    rotationInterval={2500}
+                                />
+                            </div>
+                        <span className="block">Products</span>
                     </h1>
 
-                    <p className="text-gray-400 max-w-2xl text-lg mx-auto mb-12">
-                        Membangun ekosistem digital terstruktur untuk ambisi bisnis Anda.
+                    <p className="text-gray-400/80 max-w-xl text-lg mx-auto mb-12 leading-relaxed">
+                       From idea to scalable digital product.
                     </p>
-
                     <div className="flex gap-5 justify-center">
-                        <button className="px-10 py-5 bg-white text-brand-darkbrown font-black rounded-full uppercase text-sm hover:scale-105 transition-transform">
+                        <button className="px-10 py-5 bg-white text-brand-darkbrown font-black rounded-full uppercase text-sm hover:scale-105 transition-all shadow-2xl shadow-white/5 active:scale-95">
                             Start Project →
                         </button>
                     </div>
